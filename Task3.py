05a5b3c9c9f627e4c6e4600cbfec210f7e44052e
DimaGapeev <73665012+dimagapey@users.noreply.github.com>
2020-11-30 19:22:16 +0200

    Исправил

--- Task3.py
+++ Task3.py
@@ -7,15 +7,15 @@ URL = 'http://api.openweathermap.org/data/2.5/forecast/daily'
 
 def return_weather(days=1):
     data = {'q': 'Odesa', 'units': 'metric', 'cnt': days,
-            'appid': 'f9ada9efec6a3934dad5f30068fdcbb8s'}
+            'appid': 'f9ada9efec6a3934dad5f30068fdcbb8'}
     r = requests.get(URL, data)
     return r.json()
 
 
 result = return_weather(days=5)
 with open('dayweather.txt', 'w') as f:
-    f.writelines(['  Date   ', '   Day   ', '  Night  '])
-    for day in result['list']:   # не пойму, почему не работает
-        f.writelines([datetime.date.fromtimestamp(day['dt']).strftime('%D-%M-%Y') + (5 * ' '),
+    f.writelines(['  Date   ', '     Day   ', '    Night  ', '\n '])
+    for day in result['list']:
+        f.writelines([datetime.date.fromtimestamp(day['dt']).strftime('%d-%m-%Y') + (5 * ' '),
                      str(day['temp']['day']) + (5 * ' '),
-                     str(day['temp']['night']) + (5 * ' '), '/n'])
+                     str(day['temp']['night']) + (5 * ' '), '\n'])
